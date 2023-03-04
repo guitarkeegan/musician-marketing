0b914921742c70ae92c19c99fe309015deb15dc6
guitarkeegan <keegananglim@gmail.com>
2023-03-03 16:57:43 -0800

    working on carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,19 +1,29 @@
-import Image from "next/image"
+"use client";
+import { FaGreaterThan, FaLessThan } from "react-icons/fa"
+import Image from "next/image";
 import tempImage from "@/assets/images/temp-carousel.jpg";
+import { useState } from "react";
 type CarouselProps = {
-    media: string
-}
+  media: string;
+};
 
-export default function Carousel({media}: CarouselProps){
-   return (
-    <div className="grid grid-rows-1 grid-cols-3 md:w-[800px] text-white mt-40">
-        <div className="">go left</div>
-        <div><Image
-         alt="person spreading their arms with sunrise in the background"
-         src={tempImage}
-         height={600}
-         width={800} /></div>
-        <div>go right</div>
+export default function Carousel({ media }: CarouselProps) {
+  const [imageArr, setImageArr] = useState();
+
+  return (
+    <div>
+      <div className="grid grid-rows-1 grid-cols-6 md:w-[800px] text-white mt-40 overflow-hidden">
+        <div className="col-span-1 flex justify-center items-center"><button><FaLessThan /></button></div>
+        <div className="col-span-4">
+          <Image
+            alt="person spreading their arms with sunrise in the background"
+            src={tempImage}
+            height={600}
+            width={800}
+          />
+        </div>
+        <div className="col-span-1 flex justify-center items-center"><button><FaGreaterThan /></button></div>
+      </div>
     </div>
-   )
+  );
 }

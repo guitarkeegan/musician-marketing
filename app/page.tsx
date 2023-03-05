78ab46f3d1f0208e833ba4edb4a76fb141b08832
guitarkeegan <keegananglim@gmail.com>
2023-03-04 16:02:32 -0800

    add basic carousel

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,22 @@ import aikoProfile from "@/assets/images/aiko-headshot-wall-full-fade-black-fs8.
 import HomeTitle from "@/components/HomeTitle";
 import SocialLinks from "@/components/SocialLinks";
 import TourDates from "@/components/TourDates";
-// import Carousel from "@/components/Carousel";
+import Carousel from "@/components/GitCarousel";
+
+const videoImages = [
+  {
+    src: "https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    alt: "sun shooting through the clouds"
+  },
+  {
+    src: "https://images.unsplash.com/photo-1536782376847-5c9d14d97cc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+    alt: "wheat with red sky background"
+  },
+  {
+    src: "https://images.unsplash.com/photo-1538991383142-36c4edeaffde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80",
+    alt: "clouds with blue sky in background"
+  },
+]
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,8 +35,7 @@ export default async function Home() {
       <SocialLinks />
       {/* @ts-expect-error Server Component */}
       <TourDates />
-      
-      {/* <Carousel media={"video"} /> */}
+      <Carousel images={videoImages}/>
     </main>
   );
 }
